b5b526149ba21def3d948e87b479dc5f2fa9dce2
Nirbhay Yadav 7 <ynirbhay218@gmail.com>
2024-06-03 10:19:21 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, request,jsonify,render_template
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+scaler=pickle.load(open('models/scaler.pkl','rb'))
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='POST':
+        Temperature=float(request.form.get('Temperature'))
+        RH=float(request.form.get('RH'))
+        Ws=float(request.form.get('Ws'))
+        Rain=float(request.form.get('Rain'))
+        FFMC=float(request.form.get('FFMC'))
+        DMC=float(request.form.get('DMC'))
+        ISI=float(request.form.get('ISI'))
+        Classes=float(request.form.get('Classes'))
+        if Classes==1 or 0:
+            pass
+        else:
+            return render_template('home.html', error = 'For Classes choose 1 or 0')
+        Region=float(request.form.get('Region'))
+        if Region==1 or 0:
+            pass
+        else:
+            return render_template('home.html', error = 'For Region choose 1 or 0')
+
+        new_data_scaled = scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data_scaled)
+        return render_template('home.html', result=result[0])
+    else:
+        return render_template('home.html')
+
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5001)
